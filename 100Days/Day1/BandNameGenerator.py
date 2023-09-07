96db9fff75d6afd704eba8dd4ec47920827270f9
Kiran Manam <manamkiranyadav@gmail.com>
2023-09-07 09:22:40 +0530

    Python Day 1

---
+++ 100Days/Day1/BandNameGenerator.py
@@ -0,0 +1,27 @@
+#print('Hello World')
+#print("print('what to print')")
+
+#name=input("What is your name?\n")
+# print('Hi '+name)
+
+# print ('Hello ' + input("What is your name?\n"))
+
+# print('You name has ' + str(len(input('What is your name?\n'))) +' characters')
+
+# swap value
+# a = input('a:')
+# b = input('b:')
+
+# temp = a
+# a=b
+# b=temp
+
+
+# print('a= '+ a)
+# print('b= '+ b)
+
+
+print('Welcome to the Band Name Generator.')
+city_name=input("What's is the name of the city you grew up in? \n")
+pet_name=input('What is the name of your pet?\n')
+print('Your Band Name is '+city_name+ ' ' + pet_name)
